769ed81a5f1942299b7fb8f86411d7704c9eb5f6
Prajwol Ghimire <51044128+prajwolq@users.noreply.github.com>
2022-08-04 13:54:44 +0545

    files

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Coder", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    $('.carousel').owlCarousel({
+        margin: 50,
+        
+        responsive: {
+            1:{
+                items: 1,
+                nav: false
+            },
+        
+        }
+    });
+});
